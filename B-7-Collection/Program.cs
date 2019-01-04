02c331c727bbdd4afeef4eea22d6c92c856668d8
Fomianok Hanna <geecurr@gmail.com>
2019-01-04 17:37:45 +0300

    Changes

--- B-7-Collection/Program.cs
+++ B-7-Collection/Program.cs
@@ -11,43 +11,143 @@ namespace Base.Lesson_5
     {
         static void Main(string[] args)
         {
-            //ArrayListExample();
+            //ArrayListPoemSort();
+            //ArrayListOfSongsSort();
+            //GenericListOfSongsSort();
+            //GenericListOfNeighborSearch();
+            DictionaryOfNeighborSearch();
 
             Console.ReadLine();
         }
 
-
-
-        public static void ArrayListExample()
+        public static void ArrayListPoemSort()
         {
             var poem = new ArrayList();
+
             for (int i = 0; i < 5; i++)
             {
-                var song = new Song();
-                song.Lyrics = Console.ReadLine();
-                poem.Add(song);
+                var line = Console.ReadLine();
+                poem.Add(line);
             }
 
-            //poem.Sort();
+            Console.WriteLine();
+
             poem.RemoveAt(poem.Count - 1);
 
-            object[] myArray = poem.ToArray();
-
-            foreach (var item in myArray)
+            object[] arr = poem.ToArray();
+            foreach (var item in arr)
             {
                 Console.WriteLine(item);
             }
+
+
+        }
+
+
+        public static void ArrayListOfSongsSort()
+        {
+            var poem = new ArrayList();
+
+            for (int i = 0; i < 5; i++)
+            {
+                var song = new Song();
+                song.Lirics = Console.ReadLine();
+                poem.Add(song);
+            }
+
+            Console.WriteLine();
+
+            poem.RemoveAt(poem.Count - 1);
+
+            object[] arr = poem.ToArray();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+
 
         }
 
         public class Song
         {
-            public string Lyrics;
-
+            public string Lirics;// необходимо переопределить, т.к. выводит просто название класса
             public override string ToString()
             {
-                return this.Lyrics;
+                return this.Lirics;
             }
+
+        }
+
+        public static void GenericListOfSongsSort()
+        {
+            List<Song> songs = new List<Song>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                songs.Add(new Song() { Lirics = Console.ReadLine() });
+            }
+
+            Console.WriteLine();
+
+            foreach (Song s in songs)
+            {
+                Console.WriteLine(s.Lirics);
+            }
+
+
+        }
+
+        public static void GenericListOfNeighborSearch()
+        {
+            List<Neighbor> floorNeighbors = new List<Neighbor>();
+            floorNeighbors.Add(new Neighbor() { FullName = "Petrov", FlatNumber = 1, PhoneNumber = 7512548 });
+            floorNeighbors.Add(new Neighbor() { FullName = "Sidorov", FlatNumber = 2, PhoneNumber = 4587963 });
+            floorNeighbors.Add(new Neighbor() { FullName = "Kuzin", FlatNumber = 3, PhoneNumber = 7589638 });
+            floorNeighbors.Add(new Neighbor() { FullName = "Fomenok", FlatNumber = 4, PhoneNumber = 4512645 });
+
+            floorNeighbors.Remove(new Neighbor() { FullName = "Fomenok", FlatNumber = 4, PhoneNumber = 4512645 });
+
+            var numberofFlat = int.Parse(Console.ReadLine());
+
+            foreach (var flat in floorNeighbors)
+            {
+                if (numberofFlat == flat.FlatNumber)
+                {
+                    Console.WriteLine($"The neighor - {flat.FullName}, his/her number - {flat.PhoneNumber}");
+                }
+            }
+
+
+        }
+
+        public class Neighbor
+        {
+            public string FullName;
+            public int FlatNumber;
+            public int PhoneNumber;
+        }
+
+        public static void DictionaryOfNeighborSearch()
+        {
+            Dictionary<int, Neighbor> floorNeighbors = new Dictionary<int, Neighbor>();
+            floorNeighbors.Add(1, new Neighbor() { FullName = "Petrov", FlatNumber = 1, PhoneNumber = 7512548 });
+            floorNeighbors.Add(2, new Neighbor() { FullName = "Sidorov", FlatNumber = 2, PhoneNumber = 4587963 });
+            floorNeighbors.Add(3, new Neighbor() { FullName = "Kuzin", FlatNumber = 3, PhoneNumber = 7589638 });
+            floorNeighbors.Add(4, new Neighbor() { FullName = "Fomenok", FlatNumber = 4, PhoneNumber = 4512645 });
+
+            floorNeighbors.Remove(4);
+
+            var numberofFlat = int.Parse(Console.ReadLine());
+
+            foreach (KeyValuePair<int, Neighbor> flatKey in floorNeighbors)
+            {
+                if (numberofFlat == flatKey.Key)
+                {
+                    Console.WriteLine($"The neighor - {flatKey.Value.FullName}, his/her number - {flatKey.Value.PhoneNumber}");
+                }
+            }
+
+
         }
     }
 }
